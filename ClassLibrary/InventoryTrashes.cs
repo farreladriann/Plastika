f661010237fb605ba23eee9743ef71c8166082d7
Drafaund <faundrapratamasukma505520@mail.ugm.ac.id>
2024-09-05 13:24:07 +0700

    Modified Inventories classess and Trashes class

---
+++ ClassLibrary/InventoryTrashes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    internal class InventoryTrashes
+    {
+        public void updateTrashes(int idItems, int quantity)
+        {
+            // Update the quantity of trashes in the inventory
+        }
+
+        public void getTrashInventory(int idUser)
+        {
+            // Get the inventory of the user
+        }
+    }
+}

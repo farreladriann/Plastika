a6bbe566efe232ca920b40a435d473cab4c76803
Drafaund <faundrapratamasukma505520@mail.ugm.ac.id>
2024-09-09 11:27:21 +0700

    Added Class Account, Vendor, and Agent

--- ClassLibrary/Account.cs
+++ ClassLibrary/Account.cs
@@ -8,5 +8,40 @@ namespace ClassLibrary
 {
     internal class Account
     {
+        private int Id{ get; set; }
+        private string Username { get; set; }
+        private string Password { get; set; }
+        private string Role { get; set; }
+
+        public int IdAcc
+        {
+            get { return IdAcc; }
+            set { IdAcc = value; }
+        }
+
+        public string UsernameAcc
+        {
+            get { return Username; }
+            set { Username = value; }
+        }
+
+        public string PasswordAcc
+        {
+            get { return Password; }
+            set { Password = value; }
+        }
+
+        public string RoleAcc
+        {
+            get { return Role; }
+            set { Role = value; }
+        }
+
+
+
     }
+    
+
+
+
 }

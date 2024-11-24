87200c4082f33473143b957891174b67a8411272
farreladriann <muhamadfarreladrian@365.ugm.ac.id>
2024-11-24 22:08:51 +0700

    feat: add shadow effect, button hover animations, and fade-in transition to the main form

--- WinFormUI/HalamanAwal.Designer.cs
+++ WinFormUI/HalamanAwal.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class HalamanAwal
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,30 +13,40 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support. Do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HalamanAwal));
-            this.leftPanel = new System.Windows.Forms.Panel();
-            this.logoBox = new System.Windows.Forms.PictureBox();
-            this.titleLabel = new System.Windows.Forms.Label();
-            this.rightPanel = new System.Windows.Forms.Panel();
-            this.welcomeLabel = new System.Windows.Forms.Label();
-            this.descriptionLabel = new System.Windows.Forms.Label();
-            this.loginPanel = new System.Windows.Forms.Panel();
-            this.signInButton = new System.Windows.Forms.Button();
-            this.orLabel = new System.Windows.Forms.Label();
-            this.signUpButton = new System.Windows.Forms.Button();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.leftPanel = new Guna.UI2.WinForms.Guna2Panel();
+            this.logoBox = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.titleLabel = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.rightPanel = new Guna.UI2.WinForms.Guna2Panel();
+            this.welcomeLabel = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.descriptionLabel = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.loginPanel = new Guna.UI2.WinForms.Guna2Panel();
+            this.signInButton = new Guna.UI2.WinForms.Guna2Button();
+            this.orLabel = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.signUpButton = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2ControlBox2 = new Guna.UI2.WinForms.Guna2ControlBox();
             this.leftPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.logoBox)).BeginInit();
             this.rightPanel.SuspendLayout();
             this.loginPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.BorderRadius = 20;
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2DragControl1.TargetControl = this.leftPanel;
+            this.guna2DragControl1.UseTransparentDrag = true;
             // 
             // leftPanel
             // 
@@ -53,29 +56,34 @@
             this.leftPanel.Dock = System.Windows.Forms.DockStyle.Left;
             this.leftPanel.Location = new System.Drawing.Point(0, 0);
             this.leftPanel.Name = "leftPanel";
-            this.leftPanel.Size = new System.Drawing.Size(400, 653);
-            this.leftPanel.TabIndex = 0;
+            this.leftPanel.Size = new System.Drawing.Size(300, 500);
+            this.leftPanel.TabIndex = 1;
             // 
             // logoBox
             // 
+            this.logoBox.BackColor = System.Drawing.Color.Transparent;
+            this.logoBox.BorderRadius = 125;
             this.logoBox.Image = ((System.Drawing.Image)(resources.GetObject("logoBox.Image")));
-            this.logoBox.Location = new System.Drawing.Point(75, 200);
+            this.logoBox.ImageRotate = 0F;
+            this.logoBox.Location = new System.Drawing.Point(25, 80);
             this.logoBox.Name = "logoBox";
             this.logoBox.Size = new System.Drawing.Size(250, 250);
             this.logoBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.logoBox.TabIndex = 0;
             this.logoBox.TabStop = false;
+            this.logoBox.Click += new System.EventHandler(this.logoBox_Click);
             // 
             // titleLabel
             // 
-            this.titleLabel.Font = new System.Drawing.Font("Segoe UI", 32F, System.Drawing.FontStyle.Bold);
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold);
             this.titleLabel.ForeColor = System.Drawing.Color.White;
-            this.titleLabel.Location = new System.Drawing.Point(0, 470);
+            this.titleLabel.Location = new System.Drawing.Point(85, 340);
             this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(400, 70);
+            this.titleLabel.Size = new System.Drawing.Size(150, 56);
             this.titleLabel.TabIndex = 1;
             this.titleLabel.Text = "Plastika";
-            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.titleLabel.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // rightPanel
             // 
@@ -83,34 +91,36 @@
             this.rightPanel.Controls.Add(this.welcomeLabel);
             this.rightPanel.Controls.Add(this.descriptionLabel);
             this.rightPanel.Controls.Add(this.loginPanel);
+            this.rightPanel.Controls.Add(this.guna2ControlBox1);
+            this.rightPanel.Controls.Add(this.guna2ControlBox2);
             this.rightPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rightPanel.Location = new System.Drawing.Point(400, 0);
+            this.rightPanel.Location = new System.Drawing.Point(300, 0);
             this.rightPanel.Name = "rightPanel";
-            this.rightPanel.Padding = new System.Windows.Forms.Padding(60);
-            this.rightPanel.Size = new System.Drawing.Size(582, 653);
-            this.rightPanel.TabIndex = 1;
+            this.rightPanel.Padding = new System.Windows.Forms.Padding(40);
+            this.rightPanel.Size = new System.Drawing.Size(400, 500);
+            this.rightPanel.TabIndex = 0;
             // 
             // welcomeLabel
             // 
-            this.welcomeLabel.Font = new System.Drawing.Font("Segoe UI Light", 28F);
+            this.welcomeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.welcomeLabel.Font = new System.Drawing.Font("Segoe UI Light", 20F);
             this.welcomeLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(86)))), ((int)(((byte)(69)))));
-            this.welcomeLabel.Location = new System.Drawing.Point(60, 150);
+            this.welcomeLabel.Location = new System.Drawing.Point(40, 100);
             this.welcomeLabel.Name = "welcomeLabel";
-            this.welcomeLabel.Size = new System.Drawing.Size(480, 60);
+            this.welcomeLabel.Size = new System.Drawing.Size(284, 47);
             this.welcomeLabel.TabIndex = 0;
             this.welcomeLabel.Text = "Welcome to Plastika";
-            this.welcomeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // descriptionLabel
             // 
-            this.descriptionLabel.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.descriptionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.descriptionLabel.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.descriptionLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(109)))), ((int)(((byte)(91)))));
-            this.descriptionLabel.Location = new System.Drawing.Point(63, 220);
+            this.descriptionLabel.Location = new System.Drawing.Point(40, 155);
             this.descriptionLabel.Name = "descriptionLabel";
-            this.descriptionLabel.Size = new System.Drawing.Size(480, 120);
+            this.descriptionLabel.Size = new System.Drawing.Size(382, 25);
             this.descriptionLabel.TabIndex = 1;
-            this.descriptionLabel.Text = "Your sustainable marketplace for waste collection and recycled products. Join us " +
-    "in creating a better environment through responsible waste management.";
+            this.descriptionLabel.Text = "Your sustainable marketplace for\r\nwaste collection.";
             // 
             // loginPanel
             // 
@@ -118,90 +128,116 @@
             this.loginPanel.Controls.Add(this.signInButton);
             this.loginPanel.Controls.Add(this.orLabel);
             this.loginPanel.Controls.Add(this.signUpButton);
-            this.loginPanel.Location = new System.Drawing.Point(60, 360);
+            this.loginPanel.Location = new System.Drawing.Point(40, 250);
             this.loginPanel.Name = "loginPanel";
-            this.loginPanel.Size = new System.Drawing.Size(480, 200);
+            this.loginPanel.Size = new System.Drawing.Size(320, 150);
             this.loginPanel.TabIndex = 2;
             // 
             // signInButton
             // 
-            this.signInButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
-            this.signInButton.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.signInButton.FlatAppearance.BorderSize = 0;
-            this.signInButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.signInButton.Font = new System.Drawing.Font("Segoe UI Semibold", 12F);
+            this.signInButton.BorderRadius = 8;
+            this.signInButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.signInButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.signInButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.signInButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.signInButton.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
+            this.signInButton.Font = new System.Drawing.Font("Segoe UI Semibold", 10F);
             this.signInButton.ForeColor = System.Drawing.Color.White;
             this.signInButton.Location = new System.Drawing.Point(0, 0);
             this.signInButton.Name = "signInButton";
-            this.signInButton.Size = new System.Drawing.Size(480, 50);
+            this.signInButton.Size = new System.Drawing.Size(320, 40);
             this.signInButton.TabIndex = 0;
             this.signInButton.Text = "Sign In";
-            this.signInButton.UseVisualStyleBackColor = false;
             this.signInButton.Click += new System.EventHandler(this.signInButton_Click);
-            this.signInButton.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
-            this.signInButton.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
             // 
             // orLabel
             // 
-            this.orLabel.AutoSize = true;
-            this.orLabel.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.orLabel.BackColor = System.Drawing.Color.Transparent;
+            this.orLabel.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.orLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(109)))), ((int)(((byte)(91)))));
-            this.orLabel.Location = new System.Drawing.Point(230, 70);
+            this.orLabel.Location = new System.Drawing.Point(150, 55);
             this.orLabel.Name = "orLabel";
-            this.orLabel.Size = new System.Drawing.Size(30, 25);
+            this.orLabel.Size = new System.Drawing.Size(19, 25);
             this.orLabel.TabIndex = 1;
             this.orLabel.Text = "or";
             // 
             // signUpButton
             // 
-            this.signUpButton.BackColor = System.Drawing.Color.White;
-            this.signUpButton.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.signUpButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
-            this.signUpButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.signUpButton.Font = new System.Drawing.Font("Segoe UI Semibold", 12F);
+            this.signUpButton.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
+            this.signUpButton.BorderRadius = 8;
+            this.signUpButton.BorderThickness = 1;
+            this.signUpButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.signUpButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.signUpButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.signUpButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.signUpButton.FillColor = System.Drawing.Color.White;
+            this.signUpButton.Font = new System.Drawing.Font("Segoe UI Semibold", 10F);
             this.signUpButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
-            this.signUpButton.Location = new System.Drawing.Point(0, 110);
+            this.signUpButton.Location = new System.Drawing.Point(0, 95);
             this.signUpButton.Name = "signUpButton";
-            this.signUpButton.Size = new System.Drawing.Size(480, 50);
+            this.signUpButton.Size = new System.Drawing.Size(320, 40);
             this.signUpButton.TabIndex = 2;
             this.signUpButton.Text = "Create New Account";
-            this.signUpButton.UseVisualStyleBackColor = false;
             this.signUpButton.Click += new System.EventHandler(this.signUpButton_Click);
-            this.signUpButton.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
-            this.signUpButton.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(109)))), ((int)(((byte)(91)))));
+            this.guna2ControlBox1.Location = new System.Drawing.Point(355, 0);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox1.TabIndex = 3;
+            // 
+            // guna2ControlBox2
+            // 
+            this.guna2ControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox2.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.guna2ControlBox2.FillColor = System.Drawing.Color.White;
+            this.guna2ControlBox2.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(109)))), ((int)(((byte)(91)))));
+            this.guna2ControlBox2.Location = new System.Drawing.Point(304, 0);
+            this.guna2ControlBox2.Name = "guna2ControlBox2";
+            this.guna2ControlBox2.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox2.TabIndex = 4;
             // 
             // HalamanAwal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(982, 653);
+            this.ClientSize = new System.Drawing.Size(700, 500);
             this.Controls.Add(this.rightPanel);
             this.Controls.Add(this.leftPanel);
             this.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "HalamanAwal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Plastika";
             this.leftPanel.ResumeLayout(false);
+            this.leftPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.logoBox)).EndInit();
             this.rightPanel.ResumeLayout(false);
+            this.rightPanel.PerformLayout();
             this.loginPanel.ResumeLayout(false);
             this.loginPanel.PerformLayout();
             this.ResumeLayout(false);
+
         }
 
-        #endregion
-
-        private System.Windows.Forms.Panel leftPanel;
-        private System.Windows.Forms.PictureBox logoBox;
-        private System.Windows.Forms.Label titleLabel;
-        private System.Windows.Forms.Panel rightPanel;
-        private System.Windows.Forms.Label welcomeLabel;
-        private System.Windows.Forms.Label descriptionLabel;
-        private System.Windows.Forms.Panel loginPanel;
-        private System.Windows.Forms.Button signInButton;
-        private System.Windows.Forms.Label orLabel;
-        private System.Windows.Forms.Button signUpButton;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+        private Guna.UI2.WinForms.Guna2Panel leftPanel;
+        private Guna.UI2.WinForms.Guna2PictureBox logoBox;
+        private Guna.UI2.WinForms.Guna2HtmlLabel titleLabel;
+        private Guna.UI2.WinForms.Guna2Panel rightPanel;
+        private Guna.UI2.WinForms.Guna2HtmlLabel welcomeLabel;
+        private Guna.UI2.WinForms.Guna2HtmlLabel descriptionLabel;
+        private Guna.UI2.WinForms.Guna2Panel loginPanel;
+        private Guna.UI2.WinForms.Guna2Button signInButton;
+        private Guna.UI2.WinForms.Guna2HtmlLabel orLabel;
+        private Guna.UI2.WinForms.Guna2Button signUpButton;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox2;
     }
 }
+
--- WinFormUI/HalamanAwal.cs
+++ WinFormUI/HalamanAwal.cs
@@ -1,72 +1,141 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using WinFormUI;
+using Guna.UI2.WinForms;
 
 namespace AddProdukdanSampah
 {
     public partial class HalamanAwal : Form
     {
+        private Guna2ShadowForm guna2ShadowForm1;
+
         public HalamanAwal()
         {
             InitializeComponent();
+            ConfigureForm();
+        }
+
+        private void ConfigureForm()
+        {
+            // Add shadow effect to the form
+            this.guna2ShadowForm1 = new Guna2ShadowForm(this.components);
+            this.guna2ShadowForm1.SetShadowForm(this);
+            this.guna2ShadowForm1.BorderRadius = 20;
+            this.guna2ShadowForm1.ShadowColor = Color.FromArgb(39, 174, 96);
+
+            // Configure hover effects for buttons
+            ConfigureButtonHoverEffects();
+
+            // Add form load animations
+            this.Load += Form_Load;
+        }
+
+        private void ConfigureButtonHoverEffects()
+        {
+            // Sign In button hover effects
+            signInButton.MouseEnter += (s, e) =>
+            {
+                signInButton.FillColor = Color.FromArgb(46, 204, 113);
+                signInButton.Cursor = Cursors.Hand;
+            };
+
+            signInButton.MouseLeave += (s, e) =>
+            {
+                signInButton.FillColor = Color.FromArgb(39, 174, 96);
+            };
+
+            // Sign Up button hover effects
+            signUpButton.MouseEnter += (s, e) =>
+            {
+                signUpButton.FillColor = Color.FromArgb(39, 174, 96);
+                signUpButton.ForeColor = Color.White;
+                signUpButton.Cursor = Cursors.Hand;
+            };
+
+            signUpButton.MouseLeave += (s, e) =>
+            {
+                signUpButton.FillColor = Color.White;
+                signUpButton.ForeColor = Color.FromArgb(39, 174, 96);
+            };
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            // Initially hide elements for fade in effect
+            welcomeLabel.Visible = false;
+            descriptionLabel.Visible = false;
+            loginPanel.Visible = false;
+
+            // Create fade in animation
+            Timer fadeTimer = new Timer
+            {
+                Interval = 500
+            };
+
+            fadeTimer.Tick += (s, args) =>
+            {
+                welcomeLabel.Visible = true;
+                descriptionLabel.Visible = true;
+                loginPanel.Visible = true;
+                fadeTimer.Stop();
+                fadeTimer.Dispose();
+            };
+
+            fadeTimer.Start();
         }
 
         private void signInButton_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            Login login = new Login();
-            login.Show();
+            CreateFadeOutTransition(() =>
+            {
+                this.Hide();
+                Login login = new Login();
+                login.Show();
+            });
         }
 
         private void signUpButton_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            Role role = new Role();
-            role.Show();
-        }
-
-        private void Button_MouseEnter(object sender, EventArgs e)
-        {
-            if (sender is Button button)
+            CreateFadeOutTransition(() =>
             {
-                if (button == signInButton)
-                {
-                    button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
-                }
-                else if (button == signUpButton)
-                {
-                    button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
-                    button.ForeColor = System.Drawing.Color.White;
-                }
-            }
+                this.Hide();
+                Role role = new Role();
+                role.Show();
+            });
         }
 
-        private void Button_MouseLeave(object sender, EventArgs e)
+        private void CreateFadeOutTransition(Action onComplete)
         {
-            if (sender is Button button)
+            Timer fadeTimer = new Timer
             {
-                if (button == signInButton)
+                Interval = 10
+            };
+
+            fadeTimer.Tick += (s, args) =>
+            {
+                this.Opacity -= 0.1;
+                if (this.Opacity <= 0)
                 {
-                    button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
+                    fadeTimer.Stop();
+                    fadeTimer.Dispose();
+                    onComplete?.Invoke();
                 }
-                else if (button == signUpButton)
-                {
-                    button.BackColor = System.Drawing.Color.White;
-                    button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
-                }
-            }
+            };
+
+            fadeTimer.Start();
         }
 
-        private void HalamanAwal_Load(object sender, EventArgs e)
+        private void descriptionLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void descriptionLabel_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void logoBox_Click(object sender, EventArgs e)
         {
 
         }

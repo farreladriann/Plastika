687784b7409ff1d60c9df21ac9ce5157afa06709
farreladriann <muhamadfarreladrian@365.ugm.ac.id>
2024-11-25 12:08:52 +0700

    feat: update HalamanUtamaNew form layout and enhance UI elements for better user experience

--- WinFormUI/HalamanUtamaNew.Designer.cs
+++ WinFormUI/HalamanUtamaNew.Designer.cs
@@ -48,15 +48,21 @@
             this.flpHalamanUtama.Margin = new System.Windows.Forms.Padding(7);
             this.flpHalamanUtama.MaximumSize = new System.Drawing.Size(2000, 900);
             this.flpHalamanUtama.Name = "flpHalamanUtama";
-            this.flpHalamanUtama.Size = new System.Drawing.Size(1200, 600);
+            this.flpHalamanUtama.Size = new System.Drawing.Size(764, 600);
             this.flpHalamanUtama.TabIndex = 0;
             // 
             // tbSearch
             // 
             this.tbSearch.BorderRadius = 10;
-            this.tbSearch.PlaceholderText = "Search";
+            this.tbSearch.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tbSearch.DefaultText = "";
+            this.tbSearch.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.tbSearch.Location = new System.Drawing.Point(16, 12);
+            this.tbSearch.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tbSearch.Name = "tbSearch";
+            this.tbSearch.PasswordChar = '\0';
+            this.tbSearch.PlaceholderText = "Search";
+            this.tbSearch.SelectedText = "";
             this.tbSearch.Size = new System.Drawing.Size(500, 36);
             this.tbSearch.TabIndex = 0;
             this.tbSearch.TextChanged += new System.EventHandler(this.tbSearch_TextChanged);
@@ -65,6 +71,7 @@
             // 
             this.pbMaps.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pbMaps.Image = ((System.Drawing.Image)(resources.GetObject("pbMaps.Image")));
+            this.pbMaps.ImageRotate = 0F;
             this.pbMaps.Location = new System.Drawing.Point(1100, 12);
             this.pbMaps.Name = "pbMaps";
             this.pbMaps.Size = new System.Drawing.Size(45, 44);
@@ -88,20 +95,22 @@
             this.btnAddBarang.BackColor = System.Drawing.Color.Transparent;
             this.btnAddBarang.BorderRadius = 10;
             this.btnAddBarang.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAddBarang.FillColor = System.Drawing.Color.LightGreen;
             this.btnAddBarang.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddBarang.ForeColor = System.Drawing.Color.White;
+            this.btnAddBarang.HoverState.FillColor = System.Drawing.Color.DarkGreen;
             this.btnAddBarang.Location = new System.Drawing.Point(1100, 700);
             this.btnAddBarang.Name = "btnAddBarang";
             this.btnAddBarang.Size = new System.Drawing.Size(120, 50);
             this.btnAddBarang.TabIndex = 0;
             this.btnAddBarang.Text = "Add Barang";
-            this.btnAddBarang.FillColor = System.Drawing.Color.LightGreen;
-            this.btnAddBarang.HoverState.FillColor = System.Drawing.Color.DarkGreen;
             this.btnAddBarang.Click += new System.EventHandler(this.btnAddBarang_Click);
             // 
             // pbProfile
             // 
             this.pbProfile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pbProfile.Image = ((System.Drawing.Image)(resources.GetObject("pbProfile.Image")));
+            this.pbProfile.ImageRotate = 0F;
             this.pbProfile.Location = new System.Drawing.Point(1150, 12);
             this.pbProfile.Name = "pbProfile";
             this.pbProfile.Size = new System.Drawing.Size(44, 44);
@@ -113,11 +122,11 @@
             // lblUsername
             // 
             this.lblUsername.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblUsername.AutoSize = true;
+            this.lblUsername.BackColor = System.Drawing.Color.Transparent;
             this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblUsername.Location = new System.Drawing.Point(1200, 28);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(78, 16);
+            this.lblUsername.Size = new System.Drawing.Size(74, 18);
             this.lblUsername.TabIndex = 5;
             this.lblUsername.Text = "Username";
             // 
